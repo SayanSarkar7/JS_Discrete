75d2a63af5e356b3383047d10005997d2aeec4e5
SayanSarkar7 <sarkarsayan602@gmail.com>
2024-10-01 22:32:45 +0530

    promises updated

--- src/Components/Topic.jsx
+++ src/Components/Topic.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 const Topic = () => {
     const a=new Promise((resolve,reject)=>{
         resolve("promise fulfilled");
+        reject("some technical error")
     });
     a.then((params)=>{
         console.log(params, "aaa");
+        
+    },(error)=>{
+        console.log(error);
         
     })
   return (
